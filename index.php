36e68c9808c2105ca929f65a9a0b34e5b3ea70f0
Joey Sagnes <dragonskymine@gmail.com>
2018-05-17 09:33:21 +0200

    Les fichiers de base d'un site internet
    index.php / style.css / script.js

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>M2R_info</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" charset="utf-8"></script>
+  </head>
+  <body>
+    <h1>Bonjour</h1>
+  </body>
+</html>
